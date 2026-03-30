d51c4b14feb37f85f10f414b409872a2b8df8c5c
Robin Gagnon <me@reobin.dev>
2026-03-29 19:29:31 -0500

    chore: add terminal emulator and tmux extras

---
+++ extras/wezterm/olive-crt-dark.lua
@@ -0,0 +1,29 @@
+return {
+  foreground = '#FBFCF6',
+  background = '#1D211A',
+  cursor_bg = '#FBFCF7',
+  cursor_fg = '#1D211A',
+  selection_fg = '#FBFCF6',
+  selection_bg = '#616B5C',
+
+  ansi = {
+    '#1D211A',
+    '#EA938B',
+    '#BCCD80',
+    '#F7C775',
+    '#90DDE4',
+    '#DAC3ED',
+    '#A5D6CB',
+    '#FBFCF6',
+  },
+  brights = {
+    '#99A293',
+    '#EA938B',
+    '#ECF6AE',
+    '#FBE09F',
+    '#90DDE4',
+    '#DAC3ED',
+    '#A5D6CB',
+    '#FBFCF7',
+  },
+}
